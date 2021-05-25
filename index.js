8e0141e14820f35efe0ca332aba2879bd29aa589
CodingBlood <kartik.agarwal.192.168@gmail.com>
2021-05-25 17:23:37 +0530

    SignUp, Hashing, Salting Functions

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+//impoting libraries and configuring them
 const express = require("express");
 const app = express();
+const bcrypt = require('bcrypt');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const e = require("express");
+app.use(express.urlencoded({encoded: true}))
+app.engine('ejs',ejsMate)
+// Declaring Folders for css and image files
 app.use( express.static( "public" ) );
 app.use( express.static("views/CSS") );
-app.engine('ejs',ejsMate)
+
+
+// User Authentication via Hashing and Salting
+const hashpassword = async(pw)=>{
+    const salt = await bcrypt.genSalt(12);
+    const hash = await bcrypt.hash(pw, salt);
+    console.log(salt);
+    console.log(hash);
+}
+const login = async(pw, hashpassword)=>{
+    const result = await bcrypt.compare(pw, hashpassword)
+    if(result){
+        console.log("Match");
+    }else{
+        console.log("TRY AGAIN");
+    }
+}
+login("monkey","$2b$10$1V5zFenClUv1S80NL80Nx.3fi.G7/shWdVRzLPFE0poXdDM8RFFce")
+
+// Routes
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'))
 app.get('/', (req,res)=>{
@@ -12,6 +37,13 @@ app.get('/', (req,res)=>{
 })
 app.get('/Login', (req,res)=>{
     res.render('login')
+})
+app.post('/SignUp',(req,res)=>{
+    console.log(req.body)
+    res.render('login')
+})
+app.get('/SignUp',(req,res)=>{
+    res.render('signup')
 })
 app.listen(3000, ()=>{
     console.log("Server Started!")
